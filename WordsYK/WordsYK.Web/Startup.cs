102c1079326f4ded44a2458937651c3dcefe83c1
yordan89k <54133275+yordan89k@users.noreply.github.com>
2019-12-10 13:56:59 +0200

    Solution and Projects set up

---
+++ WordsYK/WordsYK.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WordsYK.Web.Startup))]
+namespace WordsYK.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
