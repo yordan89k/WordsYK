2ea71528db7db43954f1c6673a7c06d18db17e0d
yordan89k <54133275+yordan89k@users.noreply.github.com>
2019-12-23 19:52:39 +0200

    Created some basic models (such as Word, Category) and the InMemory repository with its methods

---
+++ WordsYK/WordsYK.Core/Models/Base.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsYK.Core.Models
+{
+    public abstract class Base
+    {
+        public string Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public Base()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.CreatedAt = DateTime.Now;
+        }
+
+    }
+}
---
+++ WordsYK/WordsYK.Core/Models/WordCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsYK.Core.Models
+{
+    public class WordCategory : Base
+    {
+        public string Category { get; set; }
+    }
+}
