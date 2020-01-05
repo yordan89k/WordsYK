907bdb88d32aa6645f52c70850069e400c600741
yordan89k <54133275+yordan89k@users.noreply.github.com>
2020-01-05 19:51:23 +0100

    Half way done. Created the controller, added Views without editing them yet. Next add View Model, update Controller, edit views.

---
+++ WordsYK/DataInMemory/WordCategoryRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using WordsYK.Core.Models;
+
+namespace DataInMemory
+{
+    public class WordCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<WordCategory> wordCategories;
+
+        public WordCategoryRepository()
+        {
+            wordCategories = cache["wordCategories"] as List<WordCategory>;
+            if (wordCategories == null)
+            {
+                new List<WordCategory>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["wordCategories"] = wordCategories;
+        }
+
+        public void Insert(WordCategory wcat)
+        {
+            wordCategories.Add(wcat);
+        }
+
+        public void Update(WordCategory wordCategory)
+        {
+            WordCategory wordCategoryToUpdate = wordCategories.Find(wcat => wcat.Id == wordCategory.Id);
+
+            if (wordCategoryToUpdate != null)
+            {
+                wordCategoryToUpdate = wordCategory;
+            }
+            else
+            {
+                throw new Exception("Word Category not found");
+            }
+        }
+
+        public WordCategory Find(string Id)
+        {
+            WordCategory wordCategory = wordCategories.Find(wcat => wcat.Id == Id);
+
+            if (wordCategory != null)
+            {
+                return wordCategory;
+            }
+            else
+            {
+                throw new Exception("Word Category not found");
+            }
+        }
+
+        public IQueryable<WordCategory> Collection()
+        {
+            return wordCategories.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            WordCategory wordCategoryToDelete = wordCategories.Find(wcat => wcat.Id == Id);
+
+            if (wordCategoryToDelete != null)
+            {
+                wordCategories.Remove(wordCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception("Word Category not found");
+            }
+        }
+
+
+    }
+
+}
+
--- WordsYK/WordsYK.Web/Controllers/WordCategoryManagerController.cs
+++ WordsYK/WordsYK.Web/Controllers/WordCategoryManagerController.cs
@@ -4,11 +4,117 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WordsYK.Core.Models;
+using System.IO;
+using DataInMemory;
 
-namespace WordsYK.Web.Controllers
+namespace WordsYK.WebUI.Controllers
 {
     public class WordCategoryManagerController : Controller
     {
+        WordCategoryRepository context;
 
+        public WordCategoryManagerController()
+        {
+            context = new WordCategoryRepository();
+        }
+
+        public ActionResult Index()
+        {
+            List<WordCategory> wordCategories = context.Collection().ToList();
+            return View(wordCategories);
+        }
+
+        public ActionResult Create()
+        {
+            var wordCategory = new WordCategory();
+            return View(wordCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Create(WordCategory wordCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(wordCategory);
+            }
+            else
+            {
+                context.Insert(wordCategory);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            WordCategory wordCategory = context.Find(Id);
+            if (wordCategory == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(wordCategory);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(WordCategory wordCategory, string Id, HttpPostedFileBase file)
+        {
+            WordCategory wordCategoryToEdit = context.Find(Id);
+
+            if (wordCategoryToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(wordCategory);
+                }
+
+                wordCategoryToEdit.Category = wordCategory.Category;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            WordCategory wordToDelete = context.Find(Id);
+
+            if (wordToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(wordToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            WordCategory wordCategoryToDelete = context.Find(Id);
+
+            if (wordCategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
