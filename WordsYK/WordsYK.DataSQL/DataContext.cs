fcdbe445efa33d6b74586d1a2948eedb686115b8
yordan89k <54133275+yordan89k@users.noreply.github.com>
2020-01-10 22:49:28 +0100

    Added Db in localhost with Entity

---
+++ WordsYK/WordsYK.DataSQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordsYK.Core.Models;
+
+namespace WordsYK.DataSQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+             : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Word> Words { get; set; }
+        public DbSet<WordCategory> WordCategories { get; set; }
+
+    }
+}
---
+++ WordsYK/WordsYK.DataSQL/Migrations/202001102146387_Initial.cs
@@ -0,0 +1,41 @@
+namespace WordsYK.DataSQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.WordCategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Category = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Words",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        EnTranslation = c.String(maxLength: 100),
+                        SeTranslation = c.String(),
+                        Category = c.String(),
+                        Image = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Words");
+            DropTable("dbo.WordCategories");
+        }
+    }
+}
